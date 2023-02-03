43969e6eba40b358933e0542676326fa41ef3dd8
Tetiana Herei <there@softserveinc.com>
2023-02-03 16:17:13 +0200

    improved backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-echo "Making backup of learning log files..."
-echo "###"
-DATE=`date "+%Y-%m-%d__%H-%M-%S"`
-FILE_NAME="/home/blindguardian/backup/ll_dump_${DATE}.tar.gz"
-tar -zcf $FILE_NAME /home/blindguardian/learning_django/learning_log/
+#checking for arguments
+if [[ -z "$1"]] 
+then
+    echo "Script usage: backup.sh <full_path_to_directory>"
+    exit 1
+fi
+
+#setting variables
+DATE=`date "+%Y-%m-%d"`
+BACKUP="$(getent passwd "$USER" | cut -d: -f6)/backup"
+
+#checking if directory exists
+for DIR in "$@"
+do 
+    if [[ ! -d "$DIR" ]]
+    then
+        echo "[ERR] Not a directory or the directory doesn't exist"
+        exit 1
+    fi
+done 
+
+#checking if backup directory exists and creating it
+if [[ ! -d "$BACKUP" ]]
+then 
+    echo "Creating $BACKUP directory.."
+    mkdir -p ~/backup
+fi
+
+#making backup
+echo "Making backup..."
+for DIR in "$@"
+do
+    FILE_NAME="$BACKUP/$(basename $DIR)_$DATE.tar.gz"
+    tar -zcf "$FILE_NAME" "$DIR"  
+done
+echo "#########"
 echo "Backup is made."

9880efff0ea4e0a55efb3ce38a391c3e71362bc2
Tetiana Herei <there@softserveinc.com>
2023-02-09 11:55:29 +0200

    now python function displays only platform

--- python_function/myFunction/__init__.py
+++ python_function/myFunction/__init__.py
@@ -1,5 +1,5 @@
 import logging
-
+import re
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -8,6 +8,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     name = req.params.get('name')
     if not name:
         try:
+            # looking for name in req body
             req_body = req.get_json()
         except ValueError:
             pass
@@ -15,18 +16,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     headers = dict(req.headers)
-    agent = headers.get("user-agent")
     host = headers.get("x-forwarded-for")
-
- #   if host == "":
- #       host = headers.get("host")
- #   else:
- #       host = "[Can't detect :(]"
+    agent = headers.get("user-agent")
+    agent = re.split(r"(.*? \()(.*?)(\).*)", agent)[2]
+    
+    if host == None:
+        host = headers.get("host")
 
     if name:
-        return func.HttpResponse(f"Hello, {name}.\nYour IP is {host} and you are using:\n{agent}.")
+        return func.HttpResponse(f"Hello, {name}.\nYour IP is {host} and you are using: {agent}.")
     else:
         return func.HttpResponse(
-            "HTTP function executed successfuly. Usage: pass a name on the query string or in the request body",
+            "HTTP function executed successfully. Usage: pass a name on the query string or in the request body",
             status_code=200
         )

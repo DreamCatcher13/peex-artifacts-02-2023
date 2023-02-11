0cd2c5c5cfa484778333769b3c608441a3ebf909
Tetiana Herei <there@softserveinc.com>
2023-02-11 18:58:24 +0200

    added location detection to python function

--- python_function/myFunction/__init__.py
+++ python_function/myFunction/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import requests
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -19,12 +20,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     host = headers.get("x-forwarded-for")
     agent = headers.get("user-agent")
     agent = re.split(r"(.*? \()(.*?)(\).*)", agent)[2]
-    
+        
     if host == None:
         host = headers.get("host")
 
+    ip, port = re.split(r"(.*?):(.*?)", host)[1], re.split(r"(.*?):(.*?)", host)[3]
+    location_resp = requests.get(f'https://ipapi.co/{ip}/json/').json()
+
     if name:
-        return func.HttpResponse(f"Hello, {name}.\nYour IP is {host} and you are using: {agent}.")
+        return func.HttpResponse(f"""Hello, {name}.\nYour IP is {ip}, your port is {port} 
+and you are using: {agent}. 
+You are located in {location_resp['city']}, {location_resp['country_name']}. """)
     else:
         return func.HttpResponse(
             "HTTP function executed successfully. Usage: pass a name on the query string or in the request body",

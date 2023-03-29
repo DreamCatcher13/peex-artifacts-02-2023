30e20759e1ecb8c6117a8862df742efe309e4abe
Tetiana Herei <there@softserveinc.com>
2023-03-29 10:49:49 +0300

    function will generate a password for user

--- python_function/myFunction/__init__.py
+++ python_function/myFunction/__init__.py
@@ -1,7 +1,19 @@
 import logging
-import re
+import re, random, string
 import requests
 import azure.functions as func
+
+def generate_password():
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    min_length = random.randint(12, 16)
+    all_char = letters + digits + special
+    pwd = ""
+    while len(pwd) != min_length:
+        new_char = random.choice(all_char)
+        pwd += new_char
+    return pwd
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -27,12 +39,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     ip, port = re.split(r"(.*?):(.*?)", host)[1], re.split(r"(.*?):(.*?)", host)[3]
     location_resp = requests.get(f'https://ipapi.co/{ip}/json/').json()
 
+    password = generate_password()
+
     if name:
         return func.HttpResponse(f"""Hello, {name}.\nYour IP is {ip}, your port is {port} 
 and you are using: {agent}. 
-You are located in {location_resp['city']}, {location_resp['country_name']}. """)
+You are located in {location_resp['city']}, {location_resp['country_name']}.
+Randomly generated password: {password} """)
     else:
         return func.HttpResponse(
-            "HTTP function executed successfully. Usage: pass a name on the query string or in the request body",
+            f"""HTTP function executed successfully. Usage: pass a name on the query string or in the request body.
+Randomly generated password: {password}""",
             status_code=200
         )
